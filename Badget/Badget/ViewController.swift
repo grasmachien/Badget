a38eee2918ea36a5c75faf3f63bde7d53b135ed4
matthiasbrodelet <matthiasbrodelet@hotmail.com>
2015-06-01 14:19:05 +0200

    xcode project gestart

---
+++ Badget/Badget/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Badget
+//
+//  Created by Matthias Brodelet on 1/06/15.
+//  Copyright (c) 2015 Brodelet.Matthias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
